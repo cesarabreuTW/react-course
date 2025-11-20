779da820f2b3aefacb05626cb2f345a72fa13577
cesarabreuTW <cesar.abreu@thoughtworks.com>
2025-11-20 16:43:43 -0300

    updated lib UI components

--- libs/design-system/src/lib/components/button/button.tsx
+++ libs/design-system/src/lib/components/button/button.tsx
@@ -1,8 +1,8 @@
 import styles from './button.module.css';
 
-export function Button({ text }: { text: string }) {
+export function Button({text, onClick}: {text: string, onClick: () => void}) {
   return (
-    <button className={styles['button']}>
+    <button className={styles['container']} onClick={onClick}>
       {text}
     </button>
   );
--- libs/design-system/src/lib/components/modal/Modal.tsx
+++ libs/design-system/src/lib/components/modal/Modal.tsx
@@ -1,42 +1,47 @@
-import { useState } from "react";
-import styled from "styled-components";
-
+import styled from 'styled-components';
 
 const ModalBackground = styled.div`
-position: absolute;
-left: 0;
-top:0;
-overflow: auto;
-background-color: rgba(0, 0, 0, 0.5);
-width: 100%;
-height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
 `;
 const ModalContent = styled.div`
-margin: 12% auto;
-padding: 24px;
-background-color: wheat;
-width: 50%;
+  margin: 12% auto;
+  padding: 24px;
+  background-color: wheat;
+  width: 50%;
 `;
 
+const CloseIcon = styled.i`
+  cursor: pointer;
+  font-size: 24px;
+`;
 
-
-export const Modal: React.FunctionComponent<{ children: React.ReactNode }> = ({ children }) => {
-
-    const [show, setShow] = useState(false);
-
-    return (
-        <>
-            <button onClick={() => setShow(true)}>show modal</button>
-            {
-                show && (<ModalBackground onClick={() => setShow(false)}>
-                    <ModalContent onClick={e => e.stopPropagation()}>
-                        <i style={{ cursor: "pointer" }} className="material-icons" onClick={() => setShow(false)}>close</i>
-                        {children}
-                    </ModalContent>
-                </ModalBackground>)
-            }
-        </>
-    )
-}
+export const Modal: React.FunctionComponent<{
+  children: React.ReactNode;
+  shouldDisplay: boolean;
+  onClose: () => void;
+}> = ({ children, shouldDisplay, onClose }) => {
+  return (
+    <>
+      {shouldDisplay && (
+        <ModalBackground onClick={onClose}>
+          <ModalContent onClick={(e) => e.stopPropagation()}>
+            <section style={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <CloseIcon className="material-icons" onClick={onClose}>
+                close
+              </CloseIcon>
+            </section>
+            {children}
+          </ModalContent>
+        </ModalBackground>
+      )}
+    </>
+  );
+};
 
 export default Modal;
