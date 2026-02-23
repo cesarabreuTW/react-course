e3e66ada34305335917a1c9d4567697671e9f2d8
cesarabreuTW <cesar.abreu@thoughtworks.com>
2026-02-23 18:21:17 -0300

    feature(nebula-react-header): added header component from nebula-react

--- apps/layout/src/app/app.tsx
+++ apps/layout/src/app/app.tsx
@@ -2,22 +2,29 @@
 // "exports": {
 //   "./styles": "./dist/index.css"
 // }
+import { Header } from "@emx-mantto/nebula-react";
+import "@emx-mantto/nebula-react/styles.css";
 import { ResorceLoaderRender } from '@react-course/design-system';
 import '@react-course/design-system/styles';
 import axios from 'axios';
 import { Books } from './components/books/books';
 
 async function fetchData(url: string) {
-  const response = await axios.get('/api/books');
+  const response = await axios.get(url);
   return response.data;
 }
 
 function App() {
   return (
-    <ResorceLoaderRender
-      getData={() => fetchData('/api/books')}
-      render={(books) => <Books books={books} />}
-    />
+<>
+<Header user={{
+    name: 'Jane Doe'
+  }} />
+<ResorceLoaderRender
+  getData={() => fetchData('/api/books')}
+  render={(books) => <Books books={books} />}
+/>
+</>
   );
 }
 
