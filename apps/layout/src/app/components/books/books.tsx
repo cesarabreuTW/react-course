ab347e6d6eeb8e6191ff8e1bfe9f4ea82a9735ff
cesarabreuTW <cesar.abreu@thoughtworks.com>
2026-04-29 12:46:31 -0400

    feature(Books): added UI components

--- apps/layout/src/app/components/books/books.tsx
+++ apps/layout/src/app/components/books/books.tsx
@@ -1,9 +1,8 @@
-import { Button, Modal } from '@react-course/design-system';
+import { Button, Card, Modal } from '@react-course/design-system';
 import { useState } from 'react';
+import { logBookData } from '../../hoc/books';
 import { Book } from './book/book';
 import styles from './books.module.css';
-
-import { logBookData } from '../../hoc/books';
 
 // This is the high-order component that wraps the Book component. The only thing it does is to log the book data
 const HocBook = logBookData(Book);
@@ -16,32 +15,27 @@ export function Books({ books }: { books?: Record<string, any>[] }) {
   return books ? (
     <div className={styles['wrapper']}>
       {books.map((book) => (
-        <div key={book.id} className={styles['container']}>
-          {/* Its better to use a particular id of item insted of its array's index. 
+         <Card key={book.id} header={book.title} > 
+         {/* Its better to use a particular id of item insted of its array's index. 
           This is because React will render the whole list specially when items are added or removed.*/}
-          <h2>{book.title}</h2>
-          <Button
-            text="View Details"
-            onClick={() => setSelectedBookId(book.id)}
-          />
-          {/* Learning 💡:Modal this way renders all the contents at once, so we shouldn't depend on showModal state, instead we should depend on the book id to render once  */}
-          {/* <Modal shouldDisplay={showModal} onClose={() => setShowModal(false)}>
-            <img src={book.cover} alt={book.title} />
-            <h2>{book.title}</h2>
-            <h3>Released in {book.year}</h3>
-            <h3>Description</h3>
-            <p>{book.description}</p>
-            <p>This book has {book.pages} pages.</p>
-            <Button text="Close" onClick={() => setShowModal(false)} />
-          </Modal> */}
-          <Modal
-            shouldDisplay={selectedBookId === book.id}
-            onClose={() => setSelectedBookId(null)}
-          >
-            <HocBook book={book} />
-            <Button text="Close" onClick={() => setSelectedBookId(null)} />
-          </Modal>
-        </div>
+            <Button text="View Details" onClick={() => setSelectedBookId(book.id)} />
+            <Modal shouldDisplay={selectedBookId === book.id} onClose={() => setSelectedBookId(null)} >
+                {/* Learning 💡:Modal this way renders all the contents at once, so we shouldn't depend on showModal state, instead we should depend on the book id to render once  */}
+
+                {/* <Modal shouldDisplay={showModal} onClose={() => setShowModal(false)}>
+                  <img src={book.cover} alt={book.title} />
+                  <h2>{book.title}</h2>
+                  <h3>Released in {book.year}</h3>
+                  <h3>Description</h3>
+                  <p>{book.description}</p>
+                  <p>This book has {book.pages} pages.</p>
+                  <Button text="Close" onClick={() => setShowModal(false)} />
+                </Modal> */}
+
+                <HocBook book={book} />
+                <Button text="Close" onClick={() => setSelectedBookId(null)} />
+            </Modal>
+         </Card>
       ))}
     </div>
   ) : (
